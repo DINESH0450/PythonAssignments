fe98fbc17e32b0e69f4084af6f7ffbd0853c3adb
DINESH0450 <153703883+DINESH0450@users.noreply.github.com>
2024-01-18 11:40:21 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,277 @@
+#Compound Operators--Bitwise AND (&)
+D=9
+D&=3
+print(D)
+
+i=13
+i&=15
+print(i)
+
+a=50
+a&=30
+print(a)
+
+b=30
+b&=20
+print(b)
+
+c=15
+c&=10
+print(c)
+
+#Bitwise OR (|)
+d=5
+d|=8
+print(d)
+
+e=9
+e|=12
+print(e)
+
+f=8
+f|=8
+print(f)
+
+g=5
+g|=15
+print(g)
+
+h=6
+h|=7
+print(h)
+
+#Bitwise XOR(^)
+k=8
+k^=14
+print(k)
+
+i=12
+i^=17
+print(i)
+
+m=12
+m^=15
+print(m)
+
+n=29
+n^=30
+print(n)
+
+q=41
+q^=32
+print(q)
+
+#Bitwise NOT (~)
+t=25
+num=~t
+print(bin(u))
+print(bin(value))
+print(value)
+
+u=40
+value=~u
+print(bin(u))
+print(bin(value))
+print(value)
+
+#Bitwise RIGHT shift(>>)
+w=35
+w1=w>>3
+print(w1)
+
+z=50
+z1=z>>2
+print(z1)
+
+#compound Operators
+#Addition +=
+a=17
+a+=7
+print(a)
+
+b=7890
+b+=70
+print(b)
+
+c=345
+c+=12
+print(c)
+
+d=420
+d+=-14
+print(d)
+
+e=542
+e+=3.5
+print(e)
+
+##Subtraction -=
+A=432
+A-=25.5
+print(A)
+
+B=677
+B-=31.7
+print(B)
+
+C=5609
+C-=595.777
+print(C)
+
+D=54
+D-=45
+print(D)
+
+E=-60
+E-=30
+print(E)
+
+#Multiplication(*=)
+j=678909
+j*=56
+print(j)
+
+i=345678
+i*=67
+print(i)
+
+k=234567
+k*=98
+print(k)
+
+u=78
+u*=-87
+print(u)
+
+h=7033
+h*=87
+print(h)
+
+#Division (/)
+
+s=7890544
+s/=765
+print(s)
+
+o=78988
+o/=87
+print(o)
+
+p=-987650
+p/=789
+print(p)
+
+q=789
+q/=-9
+print(q)
+
+r=766
+r/=67
+print(r)
+
+#Modulus(%)
+v=98
+v%=7
+print(v)
+
+J=23
+J%=7
+print(J)
+
+g=2
+g%=1
+print(g)
+
+f=45
+f%=4
+print(f)
+
+x=67
+x%=6
+print(x)
+
+
+#ternary Operator
+D=35 if 40<56 else 55
+print(D)
+
+x=20
+y=50
+value= x if x>y else y
+print(value)
+
+num="even" if 23%4==o else "odd"
+print(num)
+
+a=-5
+valuea="positive"if a>o else "Negative"
+print(valuea)
+
+a,b,c=68,98,2
+abc=a if (a>b and a>c) else (b if b>c else c)
+print(abc)
+
+
+#idendity Operator
+a=50
+b=30
+print(a is b)
+
+c=[12,23,34,56]
+d=[12,23,34,56]
+print(c is not d)
+
+value='Dinesh'
+name=value
+print(value is name)
+print(value is not name)
+
+dict1={'a':1,'b':2}
+dict2={'a':1,'b':2}
+print(dict1 is dict2)
+print(dict1 is not dict2)
+
+x=[1,4,5,6,]
+y=x
+print(x is not y)
+print(x is y)
+
+
+#Membership Operators
+a=[12,34,56,78,55]
+print(34 in a)
+list=['apple','badam','orange','banana']
+print('Apple' in list)
+
+string='welcome to python world'
+print('to' in string)
+
+num=[1,3,4,5,6,7]
+print(10 not in num)
+name={'dinesh','kumar','pulakunta'}
+print('dinesh' in name)
+print('kumar'not in name)
+
+
+#input
+num=int(input("enter the number"))
+num1=num+6
+print(num 1)
+
+age=input("enter your name")
+print('hello',name)
+
+age=int(input("enter your age."))
+print("you will be"+str(age+1)+"years old next year")
+
+#output
+print('dinesh')
+print('dinesh',end='')
+print('dinesh')
+print('dinesh')
+
+
+
+
+
+
---
+++ findingBiggestNumberinTwoNumber.py
@@ -0,0 +1,7 @@
+#pro to find biggest of 2 given numbers , user input 
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+if num1>num2:
+    print(num1,"is the biggest number")
+else:
+    print(num2,"is the biggest number")
